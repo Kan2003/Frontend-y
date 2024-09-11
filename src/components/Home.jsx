3c401c0bfe3e06d88e8d9198026e4dfadeb8ba6a
Kan2003 <kanhavishwakarma2003@gmail.com>
2024-09-11 18:45:17 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <div className='w-full h-screen bg-zinc-900 text-white'>
+        <h1>hiii</h1>
+    </div>
+    </>
+  )
+}
+
+export default Home
