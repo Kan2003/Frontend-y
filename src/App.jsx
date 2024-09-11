126aafda90df07c24e5ab7b7d0df82ace11e4134
Kan2003 <kanhavishwakarma2003@gmail.com>
2024-09-11 19:08:06 +0530

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Layout from "./components/Layout/Layout";
 function App() {
-
-
   return (
     <Router>
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </div>
-  </Router>
-  )
+
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Layout>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='fixed w-full border-b-2 border-b-white h-[50px] top-0 z-50'>
+
+    </div>
+  )
+}
+
+export default Header
